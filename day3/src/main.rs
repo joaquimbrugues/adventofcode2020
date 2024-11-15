be6166415ac3cb552bf6a3f557863f373b74d108
Joaquim <devjoaquim@gmail.com>
2024-11-15 23:30:56 +0100

    Day 3, Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -25,7 +25,39 @@ fn run1(input: &str) -> u32 {
 }
 
 fn run2(input: &str) -> u32 {
-    0
+    let slopes = [(1,1), (3,1), (5,1), (7,1), (1, 2)];
+    let mut status = [(0,0), (0,0), (0,0), (0,0), (0,0)]; // First coordinate of every tuple is the
+                                                          // position in width, the second
+                                                          // coordinate is the number of trees
+                                                          // encountered
+    assert_eq!(slopes.len(), status.len());
+    let mut length = None;
+    let mut posh = 0;
+    for line in input.lines() {
+        match length {
+            None => {
+                length = Some(line.chars().count());
+            },
+            Some(l) => {
+                let mut i = 0;
+                for (right, down) in slopes {
+                    if posh % down == 0 {
+                        status[i].0 = (status[i].0 + right) % l;
+                        match line.chars().nth(status[i].0).unwrap() {
+                            '#' => {
+                                status[i].1 += 1;
+                            },
+                            '.' => {},
+                            _ => panic!("All characters should be '.' or '#'"),
+                        }
+                    }
+                    i += 1;
+                }
+            },
+        }
+        posh += 1;
+    }
+    status.iter().map(|(_, t)| t).product()
 }
 
 fn main() {
@@ -42,7 +74,7 @@ fn main() {
 
     let input = fs::read_to_string(filepath).unwrap();
 
-    let res = run1(&input);
+    let res = run2(&input);
     println!("{res}");
 }
 
@@ -67,9 +99,9 @@ fn example2() {
     assert_eq!(res, 336);
 }
 
-//#[test]
-//fn input2() {
-    //let input = fs::read_to_string("input.txt").unwrap();
-    //let res = run2(&input);
-    //assert_eq!(res,42);
-//}
+#[test]
+fn input2() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res = run2(&input);
+    assert_eq!(res, 2106818610);
+}
