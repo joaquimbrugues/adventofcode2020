f0629c76b063a17c7e08aa3f27c0a977a43cc7e6
Joaquim <devjoaquim@gmail.com>
2024-11-17 02:31:01 +0100

    Day 7, first part

---
+++ day7/src/main.rs
@@ -0,0 +1,90 @@
+use std::{env,fs,process};
+use std::collections::{HashSet, HashMap};
+
+fn run1(input: &str) -> usize {
+    // Idea: Encode the list of rules as a directed graph (a HashMap that for each key stores the
+    // number of adjacent neighbours), but directly in reverse. Then, do DFS to count every
+    // possible reachable color
+    let mut graph: HashMap<(&str, &str), Vec<(&str, &str)>> = HashMap::new();
+    for line in input.lines() {
+        let (first, second) = line.split_once(" contain ").unwrap();
+        let first = first.strip_suffix(" bags").unwrap();
+        let col = first.split_once(' ').unwrap();
+        if second != "no other bags." {
+            for chunk in second.split(", ") {
+                let aux = chunk.split_whitespace().collect::<Vec<_>>();
+                let key = (aux[1], aux[2]);
+                match graph.get_mut(&key) {
+                    Some(v) => { v.push(col) },
+                    None => { graph.insert(key, vec![col]); },
+                }
+            }
+        }
+    }
+    
+    let mut stack = vec![("shiny", "gold")];
+    let mut found = HashSet::new();
+    while let Some(col) = stack.pop() {
+        if !found.contains(&col) {
+            if col != ("shiny", "gold") {
+                found.insert(col);
+            }
+            if let Some(neighs) = graph.get(&col) {
+                for &n in neighs {
+                    stack.push(n);
+                }
+            }
+        }
+    }
+    found.len()
+}
+
+fn run2(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let mut args = env::args();
+    let filepath;
+    args.next();
+    if let Some(s) = args.next() {
+        filepath = s;
+    }
+    else {
+        eprintln!("Give me a file name! I must feeds on files! Aaargh!");
+        process::exit(1);
+    }
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    let res = run1(&input);
+    println!("{res}");
+}
+
+#[test]
+fn example1() {
+    let input = fs::read_to_string("test.txt").unwrap();
+    let res = run1(&input);
+    assert_eq!(res, 4);
+}
+
+#[test]
+fn input1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res = run1(&input);
+    assert_eq!(res, 370);
+}
+
+//#[test]
+//fn example2() {
+    //let input = fs::read_to_string("test.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
+
+//#[test]
+//fn input2() {
+    //let input = fs::read_to_string("input.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
